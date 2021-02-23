c723730822b729ac90120b75fb98fc052ccfed9a
Pavel Savinov <pavel@heady.io>
2021-02-23 16:47:31 +0100

    add permalinks for the questions

--- Answer.js
+++ Answer.js
@@ -6,7 +6,47 @@ var Answer = {
 		$('#newQuestion').val('');
 
 		alert('Спасибо за Ваш вопрос / исправление!');
+	},
+
+	showAnswer: function(data, question) {
+		var answerText = data[question];
+
+		if (answerText) {
+			$('#questionText').text(question);
+			$('#answerText').html(answerText);
+			$('#permalink').attr('href', '/?q=' + createSlug(question));
+	
+			$('#answerModal').modal('toggle');
+	
+			$('#question').val('');
+		}
 	}
+};
+
+var LETTERS = {
+    "а": "a", "б": "b", "в": "v", "г": "g",
+    "д": "d", "е": "e", "ё": "yo", "ж": "zh",
+    "з": "z", "и": "i", "й": "yi", "к": "k",
+    "л": "l", "м": "m", "н": "n", "о": "o",
+    "п": "p", "р": "r", "с": "s", "т": "t", 
+	"у": "u", "ф": "f", "х": "j", "ц": "c",
+	"ч": "ch", "ш": "sh", "щ": "sch", "ъ": "",
+	"ы": "y", "ь": "", "э": "e", "ю": "yu",
+	"я": "ya", "?": "", " ": "-"
+};
+
+var createSlug = function(input) {
+	var output = "";
+
+	var lowercase = input.toLowerCase();
+
+	for (var k = 0; k<lowercase.length; k++) {
+		var char = LETTERS[lowercase[k]] || "";
+
+		output += char;
+	}
+
+	return output;
 };
 
 $(document).ready(
@@ -28,18 +68,22 @@ $(document).ready(
 					},
 					minLength: 2,
 					select: function(event, ui) {
-						if (ui.item.value.length > 0) {
-							var answerText = data[ui.item.value];
-
-							$('#questionText').text(ui.item.value);
-							$('#answerText').html(answerText);
-
-							$('#answerModal').modal('toggle');
-
-							$('#question').val('');
+						if (ui.item.value.length) {
+							Answer.showAnswer(data, ui.item.value);
 						}
 					}
 				});
+
+				var results = (/\?q=(.*)/g).exec(window.location.search);
+				var questionSlug = (results !== null) ? results[1] || 0 : false;
+
+				if (questionSlug) {
+					var question = Object.keys(data).find(function(item) {
+						return createSlug(item) === questionSlug;
+					});
+
+					Answer.showAnswer(data, question);
+				}
 			}
 		);
 
